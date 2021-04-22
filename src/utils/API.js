3ef8c9e250423f0df1ed4103f4b3b6a63fb033a7
Your Name <you@example.com>
2021-04-21 22:31:13 -0600

    added homepage link to github pages

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const BASEURL = "hhttps://randomuser.me/";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+    return axios.get(BASEURL + query);
   }
 };
