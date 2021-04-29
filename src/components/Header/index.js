40b97bbf2d75b0a35207f3b3812e6ad5fed230a6
Dev Dork <robertcjohnson1984@gmail.com>
2021-04-28 22:56:14 -0600

    add header.css and add style to h1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './header.css';
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
 function Header({ title }) {
   return (
-    
+
       <h1>{title}</h1>
 
   );
