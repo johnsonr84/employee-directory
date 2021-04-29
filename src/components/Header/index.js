12986a04ae5ad5adf4409ad2efe6a6b0c92b9a77
Dev Dork <robertcjohnson1984@gmail.com>
2021-04-28 22:48:00 -0600

    removed unnessecary folders/files and just got header to display h1

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function Header({ title }) {
+  return (
+    
+      <h1>{title}</h1>
+
+  );
+}
+
+export default Header;
