5a49f15711e5721d83580be0857af8f1aa30fbea
Dev Dork <robertcjohnson1984@gmail.com>
2021-04-29 23:51:36 -0600

    update api.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const employeeApi = "https://randomuser.me/";
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
-    return axios.get(employeeApi + query);
+  search: function() {
+    return axios.get("https://randomuser.me/api/?results=20&nat=us'");
   }
 };
